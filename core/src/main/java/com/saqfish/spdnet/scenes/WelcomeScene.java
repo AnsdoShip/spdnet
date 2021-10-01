21d279537a6d7a03e2578792c8e4d5ad5ba07a9b
saqfish <saqfish@gmail.com>
2021-10-01 01:54:13 -0500

    version alpha-7.3

--- core/src/main/java/com/saqfish/spdnet/scenes/WelcomeScene.java
+++ core/src/main/java/com/saqfish/spdnet/scenes/WelcomeScene.java
@@ -163,8 +163,7 @@ public class WelcomeScene extends PixelScene {
 				//TODO: change the messages here in accordance with the type of patch.
 				message = "SPDNet has been patched!";
 				message += "\n";
-				message += "Better server error checks\n" ;
-				message += "UI changes\n" ;
+				message += "Asset load fix\n" ;
 				message += "Bug fixes\n" ;
 			}
 		} else {
